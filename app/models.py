188eaa8bdf6614e842fed896229fb25f4ecdb330
ivanhow <71693631+ivanhow@users.noreply.github.com>
2020-11-28 18:45:35 +0200

    Post form created and styled

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -12,4 +12,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('article-detail', args=[str(self.id)])
 
